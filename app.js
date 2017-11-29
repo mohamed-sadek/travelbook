8c9bbba19e7cc3e43ff7166592881877faedd995
Mohamed Sadek <mohamed@getyourguide.com>
2017-11-29 14:46:01 +0100

    add-trip page, get and insert

--- app.js
+++ app.js
@@ -1,16 +1,36 @@
 const Koa = require('koa');
 const Router = require('koa-router');
-const BodyParser = require("koa-bodyparser");
+const BodyParser = require('koa-bodyparser');
+const Views = require('koa-views');
 const app = new Koa();
-
+const db = require('./db')(app);
 const router = new Router();
-app.use(router.routes()).use(router.allowedMethods());
-app.use(BodyParser());
 
-router.get('/', async ctx => {
-	let name = ctx.request.body.name || "World";
-	ctx.body = `Hello ${name}!`;
+app.use(BodyParser());
+app.use(Views(__dirname + '/views', {extension: 'twig'}));
+
+router.get('/index', async ctx => {
+	let records = await ctx.app.people.find().toArray();
+
+	await ctx.render('index', {
+		records: records
+	});
 });
 
+router.get('/add-trip', async ctx => {
+	await ctx.render('add_trip');
+});
+
+
+router.post('/add-trip', async ctx => {
+	await ctx.app.people.insert(ctx.request.body);
+	let records = await ctx.app.people.find().toArray();
+
+	await ctx.render('index', {
+		records: records
+	});
+});
+
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(3000);
--- db.js
+++ db.js
@@ -1,57 +1,51 @@
 const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
 const url = 'mongodb://localhost:27017/myproject';
 
-var insertDocuments = function(db, x, callback) {
-	// Get the documents collection
-	var collection = db.collection('documents');
-	// Insert some documents
-	collection.insertMany([
-		{a : x}, {a : 2}, {a : 3}
-	], function(err, result) {
-		assert.equal(err, null);
-		assert.equal(3, result.result.n);
-		assert.equal(3, result.ops.length);
-		console.log("Inserted 3 documents into the collection");
-		callback(result);
-	});
-};
-
-var findDocuments = function(db, x, callback) {
-	// Get the documents collection
-	var collection = db.collection('documents');
-	// Find some documents
-	collection.find({'a': x}).toArray(function(err, docs) {
-		assert.equal(err, null);
-		callback(docs);
-	});
-};
-
-var removeDocument = function(db, x, callback) {
-	// Get the documents collection
-	var collection = db.collection('documents');
-	// Delete document where a is 3
-	collection.deleteMany({ a : x }, function(err, result) {
-		assert.equal(err, null);
-		assert.equal(1, result.result.n);
-		console.log("Removed the document with the field a equal to 3");
-		callback(result);
-	});
-};
-
-MongoClient.connect(url, function(error, db) {
-	if (error) {
-		console.log(error);
-	}
-
-	insertDocuments(db, function() {
-
-	});
-
-	// removeDocument(db, 2, function() {
-	// 	findDocuments(db, 2, function(docs) {
-	// 		console.log(docs);
-	// 		db.close();
+class db {
+	// let insertDocuments = function(db, x, callback) {
+	// 	// Get the documents collection
+	// 	let collection = db.collection('documents');
+	// 	// Insert some documents
+	// 	collection.insertMany([
+	// 		{a : x}, {a : 2}, {a : 3}
+	// 	], function(err, result) {
+	// 		assert.equal(err, null);
+	// 		assert.equal(3, result.result.n);
+	// 		assert.equal(3, result.ops.length);
+	// 		console.log("Inserted 3 documents into the collection");
+	// 		callback(result);
 	// 	});
-	// });
-});
+	// };
+
+	// getRecords(x) {
+	// 	MongoClient.connect(url, function (error, db) {
+	//
+	// 		let collection = db.collection('documents');
+	//
+	// 		collection.find({'a': x}).toArray(function (err, docs) {
+	// 			return docs;
+	// 		});
+	// 	});
+	// };
+
+	// let removeDocument = function(db, x, callback) {
+	// 	// Get the documents collection
+	// 	let collection = db.collection('documents');
+	// 	// Delete document where a is 3
+	// 	collection.deleteMany({ a : x }, function(err, result) {
+	// 		assert.equal(err, null);
+	// 		assert.equal(1, result.result.n);
+	// 		console.log("Removed the document with the field a equal to 3");
+	// 		callback(result);
+	// 	});
+	// };
+}
+
+module.exports = function (app) {
+	MongoClient.connect(url)
+		.then((connection) => {
+			app.people = connection.collection('documents');
+			console.log("Database connection established")
+		})
+		.catch((err) => console.error(err))
+};

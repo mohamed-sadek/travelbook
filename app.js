6e6d8b5c8ceb1dd864d07772cd8deaf96150418d
Mohamed Sadek <mohamed@getyourguide.com>
2017-11-29 15:29:49 +0100

    Gitignore

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const Koa = require('koa');
+const serve = require('koa-static');
 const Router = require('koa-router');
 const BodyParser = require('koa-bodyparser');
 const Views = require('koa-views');
@@ -7,6 +8,7 @@ const db = require('./db')(app);
 const router = new Router();
 
 app.use(BodyParser());
+app.use(serve('.'));
 app.use(Views(__dirname + '/views', {extension: 'twig'}));
 
 router.get('/index', async ctx => {
@@ -21,14 +23,11 @@ router.get('/add-trip', async ctx => {
 	await ctx.render('add_trip');
 });
 
-
 router.post('/add-trip', async ctx => {
 	await ctx.app.people.insert(ctx.request.body);
-	let records = await ctx.app.people.find().toArray();
 
-	await ctx.render('index', {
-		records: records
-	});
+	ctx.status = 200;
+	ctx.redirect('/index');
 });
 
 app.use(router.routes()).use(router.allowedMethods());
--- db.js
+++ db.js
@@ -1,50 +1,11 @@
 const MongoClient = require('mongodb').MongoClient;
-const url = 'mongodb://localhost:27017/myproject';
-
-class db {
-	// let insertDocuments = function(db, x, callback) {
-	// 	// Get the documents collection
-	// 	let collection = db.collection('documents');
-	// 	// Insert some documents
-	// 	collection.insertMany([
-	// 		{a : x}, {a : 2}, {a : 3}
-	// 	], function(err, result) {
-	// 		assert.equal(err, null);
-	// 		assert.equal(3, result.result.n);
-	// 		assert.equal(3, result.ops.length);
-	// 		console.log("Inserted 3 documents into the collection");
-	// 		callback(result);
-	// 	});
-	// };
-
-	// getRecords(x) {
-	// 	MongoClient.connect(url, function (error, db) {
-	//
-	// 		let collection = db.collection('documents');
-	//
-	// 		collection.find({'a': x}).toArray(function (err, docs) {
-	// 			return docs;
-	// 		});
-	// 	});
-	// };
-
-	// let removeDocument = function(db, x, callback) {
-	// 	// Get the documents collection
-	// 	let collection = db.collection('documents');
-	// 	// Delete document where a is 3
-	// 	collection.deleteMany({ a : x }, function(err, result) {
-	// 		assert.equal(err, null);
-	// 		assert.equal(1, result.result.n);
-	// 		console.log("Removed the document with the field a equal to 3");
-	// 		callback(result);
-	// 	});
-	// };
-}
+const URL = 'mongodb://localhost:27017/myproject';
+const COLLECTION = 'trips';
 
 module.exports = function (app) {
-	MongoClient.connect(url)
+	MongoClient.connect(URL)
 		.then((connection) => {
-			app.people = connection.collection('documents');
+			app.people = connection.collection(COLLECTION);
 			console.log("Database connection established")
 		})
 		.catch((err) => console.error(err))
